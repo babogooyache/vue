9a6aa014c2eee709e37cad207326f3e58381410b
hedyWanin <49141188+hedyWanin@users.noreply.github.com>
2021-06-06 00:31:00 +0800

    cli

---
+++ vuewebpack2/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  created() {
+    this.axios.get('https://randomuser.me/api/')
+      .then((response) => {
+      console.log(response.data)
+    })
+  },
+}
+</script>
+
+
+<style lang="scss">
+ @import "~bootstrap/scss/bootstrap";
+</style>
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
