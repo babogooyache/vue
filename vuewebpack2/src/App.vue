a56ff999ed430c54c255f5dc8894c3b279c88dee
hedyWanin <49141188+hedyWanin@users.noreply.github.com>
2021-06-06 16:57:35 +0800

    新增路由路徑及連結

--- vuewebpack2/src/App.vue
+++ vuewebpack2/src/App.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <!--<router-link class="nav-link" to="/">Home</router-link>-->
+              <router-link class="nav-link" :to="{name: 'index'}">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/page">Page</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <img src="./assets/logo.png" />
     <!--<HelloWorld/>-->
     <router-view></router-view>
     <!-- 透過router 來呈現內容 -->
@@ -8,29 +25,27 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
   },
   created() {
-    this.axios.get('https://randomuser.me/api/')
-      .then((response) => {
-      console.log(response.data)
-    })
-  },
-}
+    this.axios.get("https://randomuser.me/api/").then(response => {
+      console.log(response.data);
+    });
+  }
+};
 </script>
 
-
 <style lang="scss">
- @import "~bootstrap/scss/bootstrap";
+@import "~bootstrap/scss/bootstrap";
 </style>
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- vuewebpack2/src/router/index.js
+++ vuewebpack2/src/router/index.js
@@ -1,18 +1,25 @@
+//官方的元件
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-//官方的元件
 
-import Home from  '@/components/HelloWorld';
 //自訂的分頁元件
+import Home from "@/components/HelloWorld";
+import Page from  '@/components/pages/page';
+
 
 Vue.use(VueRouter);
 
 export default new VueRouter({
- routes:[
-     {
-         name: 'index', //元件呈現的名稱
-         path: '/index',//對應的虛擬路徑
-         component: Home,//對應的元件
-     }
- ]
+  routes: [
+    {
+      name: "index", //元件呈現的名稱
+      path: "/", //對應的虛擬路徑
+      component: Home //對應的元件
+    },
+    {
+      name: "Page", 
+      path: "/page", 
+      component: Page 
+    }
+  ]
 });
