3952b6326c0cce968b19fbdddb69514df42f53ba
hedyWanin <49141188+hedyWanin@users.noreply.github.com>
2021-06-13 21:15:43 +0800

    自定義切換路由方法
    router.pushr、router.back、router.forward

--- vuewebpack2/src/components/pages/menu.vue
+++ vuewebpack2/src/components/pages/menu.vue
@@ -1,23 +1,40 @@
 <template>
   <ul class="nav">
     <li class="nav-item">
-        <router-link class="nav-link" to="/page/">card1</router-link>
+      <router-link class="nav-link" to="/page/">card1</router-link>
     </li>
     <li class="nav-item">
-        <router-link class="nav-link" to="/page/child2">card2</router-link>
+      <router-link class="nav-link" to="/page/child2">card2</router-link>
     </li>
     <li class="nav-item">
-        <router-link class="nav-link" to="/page/child3">card3</router-link>
+      <router-link class="nav-link" to="/page/child3">card3</router-link>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link" @click.prevent="designatedLink">指定連結</a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link" @click.prevent="previousLink">上一頁</a>
+    </li>
+    <li class="nav-item">
+      <a href="#" class="nav-link" @click.prevent="nextLink">下一頁</a>
     </li>
   </ul>
-
-
 </template>
-
 <script>
 export default {
   data() {
     return {};
+  },
+  methods:{
+    designatedLink(){
+      this.$router.push('/page/child2');
+    },
+    previousLink(){
+      this.$router.back();
+    },
+    nextLink(){
+      this.$router.forward();
+    },
   }
 };
 </script>
