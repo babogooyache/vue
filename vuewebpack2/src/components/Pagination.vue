82d614f53056678744ed31ccb24ab10da15f3822
hedyWanin <49141188+hedyWanin@users.noreply.github.com>
2021-08-25 00:05:58 +0800

    Update Pagination.vue

--- vuewebpack2/src/components/Pagination.vue
+++ vuewebpack2/src/components/Pagination.vue
@@ -8,9 +8,11 @@
           <span class="sr-only">Previous</span>
         </a>
       </li>
-      <li class="page-item" v-for="page in pages.total_pages" :key="page"
+      <li class="page-item" 
+        v-for="page in pages.total_pages" :key="page"
         :class="{'active': pages.current_page == page}">
-        <a class="page-link" href="#" @click.prevent="updatePage(page)">{{ page }}</a>
+        <a class="page-link" href="#" 
+          @click.prevent="updatePage(page)">{{ page }}</a>
       </li>
       <li class="page-item" :class="{'disabled': !pages.has_next}">
         <a class="page-link" href="#" aria-label="Next"
@@ -35,7 +37,7 @@ export default {
     }
   },
   methods: {
-    //使用 emit 向外傳遞目前的事件
+    //使用 emit 向外傳遞事件
     //uploadPages 事件名
     updatePage(page) {
       this.$emit('uploadPages', page);
